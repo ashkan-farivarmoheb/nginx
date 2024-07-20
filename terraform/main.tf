aa52f13b01fe289a416e18c4c90d082eb957f258
ashkan-farivarmoheb <ashkan.farivarmoheb@gmail.com>
2024-07-21 02:43:27 +1000

    fixing upload certs to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,19 @@
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
+  force_destroy = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Principal = "*"
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::${var.bucket_name}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_acl" "my_bucket_acl" {

7313d4b6b609ab6c6eee47156420d43c71bcaf48
ashkan-farivarmoheb <ashkan.farivarmoheb@gmail.com>
2024-07-21 22:41:31 +1000

    fixing upload certs to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,8 +28,8 @@ locals {
   all_files  = flatten([local.conf_files, local.crt_files, local.key_files, local.pem_files, local.csr_files, local.srl_files])
 }
 
-resource "aws_s3_object" "upload_conf_files" {
-  for_each = fileset("../ssl", "*.conf")
+resource "aws_s3_object" "upload_files" {
+  for_each = fileset("../ssl", "*")
 
   bucket = aws_s3_bucket.my_bucket.bucket
   key    = "${var.folder_name}/${each.value}"
@@ -38,47 +38,3 @@ resource "aws_s3_object" "upload_conf_files" {
 
   depends_on = [null_resource.run_shell_script]
 }
-
-resource "aws_s3_object" "upload_crt_files" {
-  for_each = fileset("../ssl", "*.crt")
-
-  bucket = aws_s3_bucket.my_bucket.bucket
-  key    = "${var.folder_name}/${each.value}"
-  source = "../ssl/${each.value}"
-  acl    = "private"
-
-  depends_on = [null_resource.run_shell_script]
-}
-
-resource "aws_s3_object" "upload_key_files" {
-  for_each = fileset("../ssl", "*.key")
-
-  bucket = aws_s3_bucket.my_bucket.bucket
-  key    = "${var.folder_name}/${each.value}"
-  source = "../ssl/${each.value}"
-  acl    = "private"
-
-  depends_on = [null_resource.run_shell_script]
-}
-
-# resource "aws_s3_object" "upload_files" {
-#   for_each = toset(local.all_files)
-
-#   bucket = aws_s3_bucket.my_bucket.bucket
-#   key    = "${var.folder_name}/${each.value}"
-#   source = "../ssl/${each.value}"
-#   acl    = "private"
-
-#   depends_on = [null_resource.run_shell_script]
-# }
-
-# resource "aws_s3_object" "upload_files_original" {
-#   for_each = fileset("../ssl/original", "*.crt")
-
-#   bucket = aws_s3_bucket.my_bucket.bucket
-#   key    = "${var.folder_name}/${each.value}"
-#   source = "../ssl/original/${each.value}"
-#   acl    = "private"
-
-#   depends_on = [null_resource.run_shell_script]
-# }
--- terraform/output.tf
+++ terraform/output.tf
@@ -6,14 +6,6 @@ output "folder_name" {
   value = "${var.folder_name}"
 }
 
-output "upload_conf_files" {
-  value = [for obj in aws_s3_object.upload_conf_files : obj.key]
-}
-
-output "upload_crt_files" {
-  value = [for obj in aws_s3_object.upload_crt_files : obj.key]
-}
-
-output "upload_key_files" {
-  value = [for obj in aws_s3_object.upload_key_files : obj.key]
+output "upload_files" {
+  value = [for obj in aws_s3_object.upload_files : obj.key]
 }

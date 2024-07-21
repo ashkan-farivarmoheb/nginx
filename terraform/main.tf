ecc6c41c79252d436bba9acbb62b3f3fefeba8a1
ashkan-farivarmoheb <ashkan.farivarmoheb@gmail.com>
2024-07-21 22:02:13 +1000

    fixing upload certs to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,8 +28,8 @@ locals {
   all_files  = flatten([local.conf_files, local.crt_files, local.key_files, local.pem_files, local.csr_files, local.srl_files])
 }
 
-resource "aws_s3_object" "upload_files" {
-  for_each = toset(local.all_files)
+resource "aws_s3_object" "upload_conf_files" {
+  for_each = fileset("../ssl", "*.conf")
 
   bucket = aws_s3_bucket.my_bucket.bucket
   key    = "${var.folder_name}/${each.value}"
@@ -39,13 +39,46 @@ resource "aws_s3_object" "upload_files" {
   depends_on = [null_resource.run_shell_script]
 }
 
-resource "aws_s3_object" "upload_files_original" {
-  for_each = fileset("../ssl/original", "*.crt")
+resource "aws_s3_object" "upload_crt_files" {
+  for_each = fileset("../ssl", "*.crt")
 
   bucket = aws_s3_bucket.my_bucket.bucket
   key    = "${var.folder_name}/${each.value}"
-  source = "../ssl/original/${each.value}"
+  source = "../ssl/${each.value}"
   acl    = "private"
 
   depends_on = [null_resource.run_shell_script]
 }
+
+resource "aws_s3_object" "upload_key_files" {
+  for_each = fileset("../ssl", "*.key")
+
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "${var.folder_name}/${each.value}"
+  source = "../ssl/${each.value}"
+  acl    = "private"
+
+  depends_on = [null_resource.run_shell_script]
+}
+
+# resource "aws_s3_object" "upload_files" {
+#   for_each = toset(local.all_files)
+
+#   bucket = aws_s3_bucket.my_bucket.bucket
+#   key    = "${var.folder_name}/${each.value}"
+#   source = "../ssl/${each.value}"
+#   acl    = "private"
+
+#   depends_on = [null_resource.run_shell_script]
+# }
+
+# resource "aws_s3_object" "upload_files_original" {
+#   for_each = fileset("../ssl/original", "*.crt")
+
+#   bucket = aws_s3_bucket.my_bucket.bucket
+#   key    = "${var.folder_name}/${each.value}"
+#   source = "../ssl/original/${each.value}"
+#   acl    = "private"
+
+#   depends_on = [null_resource.run_shell_script]
+# }
